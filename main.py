bed15bd96fbf4bcde72b25d4d94394cd4f185603
turradeira <tiagaomatos@hotmail.com>
2023-09-11 00:39:32 +0100

    started working on the main window

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import os
+import sys
+import tkinter as tk
+
+#region Window Creator
+window = tk.Tk()
+window.title(" RSStHINGY ")
+window.geometry("600x480")
+newlabel = tk.Label(text="test")
+newlabel.grid(column=0,row=0)
+#endregion
+
+
+
+window.mainloop()
